b6563e9db59f0679baa5312bdc1c550a99c5f655
Benoît de Biolley <bdb@aloalto.com>
2025-02-05 15:42:24 +0100

    :bricks: Remove fesk and webpack

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,4 @@
 module.exports = {
-  preset: 'ts-jest/presets/js-with-ts',
-  testEnvironment: 'node',
-  coveragePathIgnorePatterns: [
-    '/node_modules/',
-    '/coverage/',
-    'dist/',
-    '(.test)\\.(ts|tsx|js)$',
-    'jest.transform.js',
-    '.json',
-  ]
+  preset: "ts-jest",
+  testEnvironment: "node"
 };
